4382297ba6c688d8ed162af634f2fe3a2ad48ca3
Your Name <you@example.com>
2022-02-13 19:25:41 +0530

    fix port

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    MONGODB:
+      'mongodb+srv://Dee2511:2511@cluster0.2gkga.mongodb.net/socialmedia?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+  };
